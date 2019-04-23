52a7866d2e795e42833ee1f221e29113883e41bb
Weizhen Wang <wangwzhen@gmail.com>
2019-04-23 15:56:58 -0500

    loop added

--- ch5_higherorderfunction.js
+++ ch5_higherorderfunction.js
@@ -6,3 +6,25 @@ let flatArray = arrays.reduce((arr1,arr2) => {
 
 console.log(flatArray);
 // → [1, 2, 3, 4, 5, 6]
+
+//Your own loop
+
+let loop = (value, testFunc, updateFunc, bodyFunc) => {
+  while(testFunc(value)) {
+    bodyFunc(value);
+    value = updateFunc(value);
+  }
+}
+
+//use for loop
+
+// function loop(value, testFunc, updateFunc, bodyFunc){
+//   for(let i=value; testFunc(i); i=updateFunc(i)) {
+//     bodyFunc(i);
+//   }
+// }
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// → 3
+// → 2
+// → 1
